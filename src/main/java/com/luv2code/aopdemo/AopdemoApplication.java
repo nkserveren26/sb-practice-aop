01353cb68c7ea0022302088853f6e559bf27e28b
nkserveren26 <norinoritoshi@outlook.jp>
2024-05-30 22:00:37 +0900

    メソッド名のみ一致のpointcut expressionの学習。

--- src/main/java/com/luv2code/aopdemo/AopdemoApplication.java
+++ src/main/java/com/luv2code/aopdemo/AopdemoApplication.java
@@ -1,6 +1,7 @@
 package com.luv2code.aopdemo;
 
 import com.luv2code.aopdemo.dao.AccountDAO;
+import com.luv2code.aopdemo.dao.MembershipDAO;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,18 +15,21 @@ public class AopdemoApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(AccountDAO theAccountDAO) {
+	public CommandLineRunner commandLineRunner(AccountDAO theAccountDAO, MembershipDAO theMembershipDAO) {
 
 		return runner -> {
 			System.out.println("Hello world");
-			demoTheBeforeAdvice(theAccountDAO);
+			demoTheBeforeAdvice(theAccountDAO, theMembershipDAO);
 		};
 	}
 
-	private void demoTheBeforeAdvice(AccountDAO theAccountDAO) {
+	private void demoTheBeforeAdvice(AccountDAO theAccountDAO, MembershipDAO theMembershipDAO) {
 
 		// call the business method
 		theAccountDAO.addAccount();
+
+		// call the membership business method
+		theMembershipDAO.addAccount();
 
 		// do it again!
 		System.out.println("\n let's call it again!\n");
