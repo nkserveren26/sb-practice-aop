0fe65256d51072dc0136d0331033179f8ae2ca0f
nkserveren26 <norinoritoshi@outlook.jp>
2024-05-30 21:48:36 +0900

    pointcut executionの学習

--- src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -14,6 +14,6 @@ public class MyDemoLoggingAspect {
 
     @Before("execution(public void addAccount())")
     public void beforeAddAccountAdvice() {
-        System.out.println("\n=====>>> Executing @Before advice on addAccount()");
+        System.out.println("\n=====>>> Executing @Before advice on method");
     }
 }
